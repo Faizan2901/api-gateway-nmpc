e8444f511d10e1c17a0f7428f681c784c250c3e6
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2024-08-10 20:42:26 +0530

    Naming-server for NMPC is added

---
+++ naming-server-playcenter/src/main/java/com/codemind/playcenter/namingserverplaycenter/NamingserverplaycenterApplication.java
@@ -0,0 +1,15 @@
+package com.codemind.playcenter.namingserverplaycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NamingserverplaycenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingserverplaycenterApplication.class, args);
+	}
+
+}
---
+++ naming-server-playcenter/src/test/java/com/codemind/playcenter/namingserverplaycenter/NamingserverplaycenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemind.playcenter.namingserverplaycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingserverplaycenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
