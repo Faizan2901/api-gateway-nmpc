4e924ca80f8c23585746d4ecf7f72c1627e2c05e
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2024-08-13 19:39:23 +0530

    Security filter check is added in Attendance service

---
+++ attendance-service/src/main/java/com/codemind/playcenter/attendanceservice/config/ApplicationProperties.java
@@ -0,0 +1,35 @@
+package com.codemind.playcenter.attendanceservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+@ConfigurationProperties(prefix = "application")
+public class ApplicationProperties {
+
+	@Value("${api.gateway.url}")
+	private String apiGatewayUrl;
+
+	@Value("${auth.secret.key}")
+	private String authSecretKey;
+
+	public String getApiGatewayUrl() {
+		return apiGatewayUrl;
+	}
+
+	public void setApiGatewayUrl(String apiGatewayUrl) {
+		this.apiGatewayUrl = apiGatewayUrl;
+	}
+
+	public String getAuthSecretKey() {
+		return authSecretKey;
+	}
+
+	public void setAuthSecretKey(String authSecretKey) {
+		this.authSecretKey = authSecretKey;
+	}
+
+}
