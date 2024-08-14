7415b2d47d6fad6b4eb69d11576a2cf5914f73e9
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2024-08-14 10:12:59 +0530

    Added error handler service for NMPC

---
+++ error-service/src/main/java/com/codemind/playcenter/errorservice/ErrorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codemind.playcenter.errorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorServiceApplication.class, args);
+	}
+
+}
